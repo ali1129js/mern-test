25577188eb75c994662be4593a8175a0411505e2
ali1129js <ali1129js@gmail.com>
2019-12-24 11:34:22 +0100

    setting up MongoDB connection

--- server/router/router.js
+++ server/router/router.js
@@ -2,17 +2,25 @@
  * @Author: Ali
  * @Date:   2019-12-23T12:18:06+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-12-23T12:21:35+01:00
+ * @Last modified time: 2019-12-23T12:40:07+01:00
  */
 const express = require('express');
 
 const router = express.Router()
 
-// define a route
+// middleware that is specific to this router
+router.use((req, res, next) =>{
+  const timeStamp = new Date().toLocaleTimeString()
+  console.log('Time: ',timeStamp )
+  next()
+})
+
+
+// define a home route
 router.get('/',(req,res) => {
   res.send('hello from express router')
 })
-// more the same
+// another route
 router.get('/about',(req,res) => {
   res.send('about express router!!')
 })
